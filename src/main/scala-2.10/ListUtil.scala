929ce80dbee0e22cbd491894ce1cfc511b039026
Thejas <thejasb@thoughtworks.com>
2017-02-27 16:57:03 +0530

    Implement ListUtil#findNth  - Returns the Nth element in list

--- src/main/scala-2.10/ListUtil.scala
+++ src/main/scala-2.10/ListUtil.scala
@@ -1,12 +1,25 @@
 object ListUtil {
   def lastNth(list: List[Int], n: Int): List[Int] = {
-    if (list.length < n )
+    if (list.length < n)
       throw new RuntimeException("Not enough item on the list")
     else if (list.isEmpty)
-       List[Int]()
-    else if(list.length == n)
-       List[Int](list.head) ++ lastNth(list.tail, n-1)
+      List[Int]()
+    else if (list.length == n)
+      List[Int](list.head) ++ lastNth(list.tail, n - 1)
     else
-       lastNth(list.tail, n)
+      lastNth(list.tail, n)
+  }
+
+  def findNth(list: List[Int], n: Int): Int = {
+    if (list.length < n)
+      throw new RuntimeException("Not enough item on the list")
+    else if (list.isEmpty)
+      throw new RuntimeException("No item on the list")
+    else if (n <= 0)
+      throw new RuntimeException(s"Invalid value for n: " + n)
+    else if (n == 1)
+      list.head
+    else
+      findNth(list.tail, n - 1)
   }
 }
--- src/test/scala-2.10/ListUtilTest.scala
+++ src/test/scala-2.10/ListUtilTest.scala
@@ -1,32 +1,62 @@
 import org.scalatest.{FlatSpec, Matchers}
 
 class ListUtilTest extends FlatSpec with Matchers {
-    "lastNth" should "return last item in the list when n = 1" in {
-      val list = List[Int](1, 2, 3, 5, 10)
-      val itemList = ListUtil.lastNth(list, 1)
-      itemList shouldEqual List[Int](10)
+  "lastNth" should "return last item in the list when n = 1" in {
+    val list = List[Int](1, 2, 3, 5, 10)
+    val itemList = ListUtil.lastNth(list, 1)
+    itemList shouldEqual List[Int](10)
+  }
+
+  "lastNth" should "return last two item in the list when n = 2" in {
+    val list = List[Int](1, 2, 3, 5, 10)
+    val itemList = ListUtil.lastNth(list, 2)
+    itemList shouldEqual List[Int](5, 10)
+  }
+
+  "lastNth" should "return an empty List if list is empty" in {
+    val list = List[Int]()
+
+    val itemList = ListUtil.lastNth(list, 0)
+    itemList.length shouldEqual 0
+  }
+
+  "lastNth" should "throw an exception when items in the list is lesser than n" in {
+    val list = List[Int]()
+
+    val expectedException = intercept[RuntimeException] {
+      ListUtil.lastNth(list, 1)
     }
 
-    "lastNth" should "return last two item in the list when n = 2" in {
-      val list = List[Int](1, 2, 3, 5, 10)
-      val itemList = ListUtil.lastNth(list, 2)
-      itemList shouldEqual List[Int](5, 10)
+    expectedException.getMessage shouldBe "Not enough item on the list"
+  }
+
+  "findNth" should "return the Nth element in the list" in {
+    val list = List[Int](1, 2, 3, 5, 10)
+    val item = ListUtil.findNth(list, 2)
+    item shouldEqual 2
+  }
+
+  "findNth" should "throw an exception when invalid value is supplied as N" in {
+    val list = List[Int](1, 2)
+    val expectedException = intercept[RuntimeException] {
+      ListUtil.findNth(list, 0)
     }
+    expectedException.getMessage shouldBe "Invalid value for n: 0"
+  }
 
-    "lastNth" should "return an empty List if list is empty" in {
-      val list = List[Int]()
-
-      val itemList =  ListUtil.lastNth(list, 1)
-      itemList.length shouldEqual 0
+  "findNth" should "throw an exception when list has less elements than the value of N" in {
+    val list = List[Int](1, 2)
+    val expectedException = intercept[RuntimeException] {
+      ListUtil.findNth(list, 3)
     }
+    expectedException.getMessage shouldBe "Not enough item on the list"
+  }
 
-    "lastNth" should "throw an exception when items in the list is lesser than n" in {
-      val list = List[Int]()
-
-      val expectedException = intercept[RuntimeException] {
-        ListUtil.lastNth(list, 1)
-      }
-
-      expectedException.getMessage shouldBe "Not enough item on the list"
+  "findNth" should "throw an exception when provided with empty list" in {
+    val list = List[Int]()
+    val expectedException = intercept[RuntimeException] {
+      ListUtil.findNth(list, 0)
     }
+    expectedException.getMessage shouldBe "No item on the list"
+  }
 }
