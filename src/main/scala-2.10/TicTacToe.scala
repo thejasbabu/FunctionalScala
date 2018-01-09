d28e9a73f2f6b5e553a79573d7fd51aedca591c2
Thejas <thejasb@thoughtworks.com>
2018-01-09 19:49:07 +0530

    Implement #vertical, #diagonal and #check partial functions

--- src/main/scala-2.10/TicTacToe.scala
+++ src/main/scala-2.10/TicTacToe.scala
@@ -1,10 +1,23 @@
 object TicTacToe {
-  def horizontal(moves: Set[Int]): Boolean = {
-    moves match {
+  val horizontal: PartialFunction[Set[Int], Boolean] = {
       case x if x.equals(Set(1, 2, 3)) => true
       case x if x.equals(Set(4, 5, 6)) => true
       case x if x.equals(Set(7, 8, 9)) => true
-      case _ => false
-    }
+  }
+
+  val vertical: PartialFunction[Set[Int], Boolean] = {
+      case x if x.equals(Set(1, 4, 7)) => true
+      case x if x.equals(Set(2, 5, 8)) => true
+      case x if x.equals(Set(3, 6, 9)) => true
+  }
+
+  val diagonal: PartialFunction[Set[Int], Boolean] = {
+      case x if x.equals(Set(1, 5, 9)) => true
+      case x if x.equals(Set(3, 5, 7)) => true
+  }
+
+  def check(moves: Set[Int]): Boolean = {
+    val winningMoves = horizontal orElse vertical orElse diagonal
+    winningMoves.isDefinedAt(moves)
   }
 }
--- src/test/scala-2.10/TicTacToeTest.scala
+++ src/test/scala-2.10/TicTacToeTest.scala
@@ -2,10 +2,33 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TicTacToeTest extends FlatSpec with Matchers {
   "horizontal" should "return true when horizontal positions are selected" in {
-    TicTacToe.horizontal(Set(1, 2, 3)) shouldEqual true
+    TicTacToe.horizontal.isDefinedAt(Set(1, 2, 3)) shouldEqual true
   }
 
   "horizontal" should "return false when set does not denote horizontal positions" in {
-    TicTacToe.horizontal(Set(1, 5, 3)) shouldEqual false
+    TicTacToe.horizontal.isDefinedAt(Set(1, 5, 3)) shouldEqual false
+  }
+
+  "vertical" should "return true when vertical positions are selected" in {
+    TicTacToe.vertical.isDefinedAt(Set(4, 1, 7)) shouldEqual true
+  }
+
+  "vertical" should "return false when set does not denote vertical positions" in {
+    TicTacToe.vertical.isDefinedAt(Set(7, 5, 3)) shouldEqual false
+  }
+
+  "diagonal" should "return true when diagonal positions are selected" in {
+    TicTacToe.diagonal.isDefinedAt(Set(9, 5, 1)) shouldEqual true
+  }
+
+  "diagonal" should "return false when set does not denote diagonal positions" in {
+    TicTacToe.diagonal.isDefinedAt(Set(1, 2, 3)) shouldEqual false
+  }
+
+  "check" should "check if the moves can win/lose a game in tic-tac-toe" in {
+    TicTacToe.check(Set(1, 2, 3)) shouldEqual true
+    TicTacToe.check(Set(1, 4, 7)) shouldEqual true
+    TicTacToe.check(Set(9, 5, 1)) shouldEqual true
+    TicTacToe.check(Set(1, 5, 3)) shouldEqual false
   }
 }
