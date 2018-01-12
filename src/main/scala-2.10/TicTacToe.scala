017eeb8506f5c115705f55e293c65795a8098392
Thejas <thejasb@thoughtworks.com>
2018-01-12 22:06:52 +0530

    Use subsetOf instead of equals

--- src/main/scala-2.10/TicTacToe.scala
+++ src/main/scala-2.10/TicTacToe.scala
@@ -1,19 +1,19 @@
 object TicTacToe {
   val horizontal: PartialFunction[Set[Int], Boolean] = {
-      case x if x.equals(Set(1, 2, 3)) => true
-      case x if x.equals(Set(4, 5, 6)) => true
-      case x if x.equals(Set(7, 8, 9)) => true
+      case x if Set(1, 2, 3).subsetOf(x) => true
+      case x if Set(4, 5, 6).subsetOf(x) => true
+      case x if Set(7, 8, 9).subsetOf(x) => true
   }
 
   val vertical: PartialFunction[Set[Int], Boolean] = {
-      case x if x.equals(Set(1, 4, 7)) => true
-      case x if x.equals(Set(2, 5, 8)) => true
-      case x if x.equals(Set(3, 6, 9)) => true
+      case x if Set(1, 4, 7).subsetOf(x) => true
+      case x if Set(2, 5, 8).subsetOf(x) => true
+      case x if Set(3, 6, 9).subsetOf(x) => true
   }
 
   val diagonal: PartialFunction[Set[Int], Boolean] = {
-      case x if x.equals(Set(1, 5, 9)) => true
-      case x if x.equals(Set(3, 5, 7)) => true
+      case x if Set(1, 5, 9).subsetOf(x) => true
+      case x if Set(3, 5, 7).subsetOf(x) => true
   }
 
   def check(moves: Set[Int]): Boolean = {
--- src/test/scala-2.10/TicTacToeTest.scala
+++ src/test/scala-2.10/TicTacToeTest.scala
@@ -2,19 +2,19 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TicTacToeTest extends FlatSpec with Matchers {
   "horizontal" should "return true when horizontal positions are selected" in {
-    TicTacToe.horizontal.isDefinedAt(Set(1, 2, 3)) shouldEqual true
+    TicTacToe.horizontal.isDefinedAt(Set(1, 2, 5, 3)) shouldEqual true
   }
 
   "horizontal" should "return false when set does not denote horizontal positions" in {
-    TicTacToe.horizontal.isDefinedAt(Set(1, 5, 3)) shouldEqual false
+    TicTacToe.horizontal.isDefinedAt(Set(1, 4, 5, 3)) shouldEqual false
   }
 
   "vertical" should "return true when vertical positions are selected" in {
-    TicTacToe.vertical.isDefinedAt(Set(4, 1, 7)) shouldEqual true
+    TicTacToe.vertical.isDefinedAt(Set(2, 4, 1, 7)) shouldEqual true
   }
 
   "vertical" should "return false when set does not denote vertical positions" in {
-    TicTacToe.vertical.isDefinedAt(Set(7, 5, 3)) shouldEqual false
+    TicTacToe.vertical.isDefinedAt(Set(7, 5, 3, 1)) shouldEqual false
   }
 
   "diagonal" should "return true when diagonal positions are selected" in {
@@ -26,9 +26,9 @@ class TicTacToeTest extends FlatSpec with Matchers {
   }
 
   "check" should "check if the moves can win/lose a game in tic-tac-toe" in {
-    TicTacToe.check(Set(1, 2, 3)) shouldEqual true
-    TicTacToe.check(Set(1, 4, 7)) shouldEqual true
-    TicTacToe.check(Set(9, 5, 1)) shouldEqual true
-    TicTacToe.check(Set(1, 5, 3)) shouldEqual false
+    TicTacToe.check(Set(1, 2, 4, 3)) shouldEqual true
+    TicTacToe.check(Set(1, 2, 4, 7)) shouldEqual true
+    TicTacToe.check(Set(3, 9, 5, 1)) shouldEqual true
+    TicTacToe.check(Set(1, 5, 4, 3)) shouldEqual false
   }
 }
